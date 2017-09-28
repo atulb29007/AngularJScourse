8ca574caea34cced94aa45b01d1d985a4ca9d99e
Atul Bishnoi <atulb29007@gmail.com>
2017-09-28 08:13:24 +0800

    second assignment solution

---
+++ module2-solution/js/app.js
@@ -0,0 +1,14 @@
+(function (){
+  "use strict";
+
+  var itemsToBuy = [
+    { name: "cookies", quantity: 10 },
+    { name: "chips", quantity: 5 },
+    { name: "pepto bismol", quantity: 4 },
+    { name: "peanuts", quantity: 2 },
+    { name: "kitkat chocolates", quantity: 6 }
+  ];
+
+  angular.module('shoppingBag',[])
+  .controller();
+})();
