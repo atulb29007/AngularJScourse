bc616d6ba71d5dbe8b1a3e74f267751b6b7e6fcb
Atul Bishnoi <atulb29007@gmail.com>
2017-11-25 10:47:27 +0530

    made a new gaming app

--- gamingApp/src/gamingApp/service.gamingDataService.js
+++ gamingApp/src/gamingApp/service.gamingDataService.js
@@ -9,19 +9,19 @@ gamingDataService.$inject = ['APIbasePath','$http'];
 function gamingDataService (APIbasePath,$http){
   var service = this;
 
-  service.getAllGames = function(){
-    var response = $http({
-      method : 'GET',
-      url : APIbasePath
-    });
-    return response;
-  };
-  // service.getAllGames = function(){
-  //   var response = $http.get('src/gamingApp/useless/gamesarena.json').success(function(res){
-  //       return res;
-  //   });
-  //   return response;
-  // };
+  //service.getAllGames = function(){
+  //  var response = $http({
+  //    method : 'GET',
+  //    url : APIbasePath
+  //  });
+  //  return response;
+  //};
+   service.getAllGames = function(){
+     var response = $http.get('src/gamingApp/useless/gamesarena.json').success(function(res){
+         return res;
+     });
+     return response;
+   };
 }
 
 })();
